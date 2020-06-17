51bb279fb267c5b948366161063deb44802ae623
drt0927 <drt_0927@naver.com>
2020-06-17 17:16:25 +0900

    create index-page

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -7,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      name: 'index-page',
+      component: require('@/components/IndexPage').default
     },
     {
       path: '*',
